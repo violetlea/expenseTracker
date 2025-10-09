6582be1ed938d1d0cef556f48ac5dff059e05fd9
violetic <tuanalianabila.aziz@gmail.com>
2025-10-09 13:47:13 +0800

    addjust btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 //import { useState } from 'react'
 import './App.css'
-import ButtonDef from './components/button'
+import ButtonDef from './components/Button'
 
 function App() {
   //const [count, setCount] = useState(0)
