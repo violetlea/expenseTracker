45dd3c6d056ba78634216c5ac6b70d6b50f35ec9
violetic <tuanalianabila.aziz@gmail.com>
2025-10-22 15:29:40 +0800

    add default message transaction list

--- src/components/RoundedDetail.jsx
+++ src/components/RoundedDetail.jsx
@@ -1,8 +1,11 @@
 export default function RoundedDetail(props) {
 
 	const {text,handleRemoveAction,index} = props;
+
 	return (
 		<>
+
+
 			<div className="rounded-lg bg-blue-500 p-2 grid grid-cols-2">
 				<div className="h-full">
 					<p key={index}>{text}</p>
--- src/features/transactions/Transactions.jsx
+++ src/features/transactions/Transactions.jsx
@@ -1,36 +1,36 @@
-import { useSelector } from "react-redux"
-import { allTransactions } from "./transactionsSlice"
+import { useSelector } from "react-redux";
+import { allTransactions } from "./transactionsSlice";
 import RoundedDetail from "../../components/RoundedDetail";
 import ManageTransaction from "../../containers/transactionsContainer";
 import { totalTrans } from "./transactionsSlice";
 import { ConvertToDecimal } from "../../helpers/helperFunction";
 
-export default function Transactions () {
+export default function Transactions() {
+	const loadTransactions = useSelector(allTransactions);
+	return (
+		<>
+			<div className="w-full">
+				<div className="grid grid-cols-1 p-2 ">
+					<div className="flex justify-between">
+						<p className="text-white">Transactions</p>
+						<p className="text-white float-right">
+							Total : {ConvertToDecimal(totalTrans)}{" "}
+						</p>
+					</div>
+				</div>
 
-    const loadTransactions = useSelector(allTransactions);
-    return (
-        <>
-            <div className="w-full">
-                <div className="grid grid-cols-1 p-2 ">
-                    <div className="flex justify-between">
-                        <p className="text-white">Transactions</p>
-                        <p className="text-white float-right">Total : {ConvertToDecimal(totalTrans)} </p>
-                    </div>
-                </div>
-                <div className="grid grid-cols-1 gap-3 md:grid-cols-2">
-                    {/* todo: to add default message if there's no transaction added */}
-                        <RoundedDetail text='test' />
-                        <RoundedDetail text='test' />
-                        <RoundedDetail text='test' />
-                        <RoundedDetail text='test' />
-                        <ManageTransaction />
-
-                </div>
-                
-                
-
-            </div>
-            
-        </>
-    )
+				{totalTrans === 0 ? (
+					<div className="p-2">
+						<p className="text-center italic text-white">
+							No transactions added yet... update your budget and add now!
+						</p>
+					</div>
+				) : (
+					<div className="grid grid-cols-1 gap-3 md:grid-cols-2">
+						<ManageTransaction />
+					</div>
+				)}
+			</div>
+		</>
+	);
 }
